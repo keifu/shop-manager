6064f9ea94b68cb8a94450193afda32ab5b525e1
keifu <keithjim2002@hotmail.com>
2017-04-17 18:16:24 +0100

    Update ShopControllerTest.java

--- src/test/java/com/shopmanager/ShopControllerTest.java
+++ src/test/java/com/shopmanager/ShopControllerTest.java
@@ -66,7 +66,9 @@ public class ShopControllerTest {
 	public void testGetNearest() throws Exception{
 		
 		shopController.getNearestShop(1.2, 1.3);
-		System.out.println("Hi");
+		
+		int a = 6;
+		int b = 8;
 		
 		verify(shopCache, times(1)).getShopList();
 		verify(shopController, times(1)).getNearestShop(anyDouble(), anyDouble(), any());
