818086536f464c774f3ef30cede4c54c3d334e26
keifu <keithjim2002@hotmail.com>
2017-04-17 18:16:03 +0100

    local commit

--- src/test/java/com/shopmanager/ShopControllerTest.java
+++ src/test/java/com/shopmanager/ShopControllerTest.java
@@ -66,9 +66,9 @@ public class ShopControllerTest {
 	public void testGetNearest() throws Exception{
 		
 		shopController.getNearestShop(1.2, 1.3);
-		System.out.println("Hi");
 		
-		System.out.println("HelloWorld");
+		int a = 2;
+		int b = 3;
 		
 		verify(shopCache, times(1)).getShopList();
 		verify(shopController, times(1)).getNearestShop(anyDouble(), anyDouble(), any());
